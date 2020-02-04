a24002129f3190a6275b8fbca2b3f4737eb1e182
Xefise <Xefise@gmail.com>
2020-02-04 14:05:44 +0300

    Add project

---
+++ Assets/Nokobot/ModernGuns_Handgun/Demo Assets/SimpleShoot.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SimpleShoot : MonoBehaviour
+{
+    public SteamVR_Action_Boolean fireAction;
+    public GameObject bulletPrefab;
+    public GameObject casingPrefab;
+    public GameObject muzzleFlashPrefab;
+    public Transform barrelLocation;
+    public Transform casingExitLocation;
+
+    private Interactable interactable;
+
+    public float shotPower = 100f;
+    public float LifeTime = 10f;
+
+    void Start()
+    {
+        if (barrelLocation == null)
+            barrelLocation = transform;
+        interactable = GetComponent<Interactable>();
+    }
+
+    void Update()
+    {
+        if (interactable.attachedToHand != null)
+        {
+            SteamVR_Input_Sources source = interactable.attachedToHand.handType;
+
+            if (fireAction[source].stateDown) Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet;
+        GameObject tempFlash;
+
+        bullet = Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+        tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
+
+        Destroy(bullet, LifeTime);
+        Destroy(tempFlash, 0.5f);
+        //  Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation).GetComponent<Rigidbody>().AddForce(casingExitLocation.right * 100f);
+
+    }
+
+    void CasingRelease()
+    {
+        GameObject casing;
+        casing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
+        casing.GetComponent<Rigidbody>().AddExplosionForce(550f, (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
+        casing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(10f, 1000f)), ForceMode.Impulse);
+    }
+}
---
+++ Assets/Skripts/ApplyDamage.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ApplyDamage : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.SendMessageUpwards("ApplyDamage", SendMessageOptions.DontRequireReceiver); // Apply damage to the balloon
+    }
+}
